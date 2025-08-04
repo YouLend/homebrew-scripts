4e03697b70bf52f102617228325ea305b193f3c2
Nikolai Main <nikolai.main@youlend.com>
2025-08-04 17:22:09 +0100

    fix - th t • Updated th t / terraform_login to pass the app into create_proxy

--- th/functions/aws.sh
+++ th/functions/aws.sh
@@ -304,6 +304,6 @@ terraform_login() {
     tsh apps logout > /dev/null 2>&1
     printf "\033[1mLogging into \033[1;32myl-admin\033[0m \033[1mas\033[0m \033[1;32msudo_admin\033[0m\n"
     tsh apps login "yl-admin" --aws-role "sudo_admin" > /dev/null 2>&1
-    create_proxy "sudo_admin"
+    create_proxy "yl-admin" "sudo_admin"
 }
 
