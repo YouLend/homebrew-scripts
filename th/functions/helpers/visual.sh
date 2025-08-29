d6064294328b8f2451ba3125a4d42a8522c28716
Nikolai Main <nikolai.main@youlend.com>
2025-08-29 16:27:31 +0100

    .

--- th/functions/helpers/visual.sh
+++ th/functions/helpers/visual.sh
@@ -198,6 +198,50 @@ print_help() {
     printf "%s          \033[0m\033[38;5;245m  ▁▁▁▁▁▁▁▁▁▁▁▁▁\033[0m\033[1;97m  ▄▄▄ ▄▁▄  \033[0m\033[38;5;245m▁▁▁▁▁▁▁▁▁▁▁▁▁▁\033[0m\033[1;34m\033[0m\n" "$center_spaces"
     printf "%s          \033[0m\033[38;5;245m ▔▔▔▔▔▔▔▔▔▔▔▔▔▔\033[0m\033[1;97m   ▀  ▀▔▀  \033[0m\033[38;5;245m▔▔▔▔▔▔▔▔▔▔▔▔▔\033[0m\033[1;34m\033[0m\n" "$center_spaces"
 }
+
+print_db_help() {
+    print "\033c"
+    create_header "th database | d"
+    printf "\033[1mConnect to our databases (RDS and MongoDB)\033[0m\n\n"
+    printf "Usage: \033[1mth database [options] | d\033[0m\n"
+    printf " ╚═ \033[1mth d\033[0m                   : Open interactive database selection.\n"
+    printf " ╚═ \033[1mth d <db-env> <port>\033[0m   : Quick database connect, Where:\n"
+    printf "    ╚═ \033[1m<db-env>\033[0m is an abbreviation for an RDS or Mongo database, using the format:\n"
+    printf "                <dbtype-env>. e.g. \033[1mr-dev\033[0m would connect to the \033[1mdev RDS cluster\033[0m\n"
+    printf "    ╚═ \033[1m<port>\033[0m   is another optional arg that allows you to specify a\n"
+    printf "                custom port for connection reuse in GUIs\n\n"
+    printf "Examples:\n"
+    printf " ╚═ $(ccode "th d r-dev")           : connects to the \033[0;32mdb-dev-aurora-postgres-1\033[0m.\n"
+    printf " ╚═ $(ccode "th d m-prod 43000")    : connects to \033[0;32mmongodb-YLProd-Cluster-1\033[0m on port \033[0;32m43000\033[0m.\n"
+}
+
+print_aws_help() {
+    print "\033c"
+    create_header "th aws | a"
+    printf "\033[1mLogin to our AWS accounts.\033[0m\n\n"
+    printf "Usage: \033[1mth aws [options] | a\033[0m\n"
+    printf " ╚═ \033[1mth a\033[0m                   : Open interactive login.\n"
+    printf " ╚═ \033[1mth a <account> <s> <b>\033[0m : Quick aws log-in, Where:\n"
+    printf "    ╚═ \033[1m<account>\033[0m is an abbreviated account name e.g. dev, cpg etc...\n"
+    printf "    ╚═ \033[1m<s>\033[0m is an optional arg which logs you in with the account's sudo role\n"
+    printf "    ╚═ \033[1m<b>\033[0m is another optional arg which opens the aws console.\n\n"
+    printf "Examples:\n"
+    printf " ╚═ $(ccode "th a dev")             : logs you into \033[0;32myl-development\033[0m as \033[1;4;32mdev\033[0m\n"
+    printf " ╚═ $(ccode "th a dev s")           : logs you into \033[0;32myl-development\033[0m as \033[1;4;32msudo_dev\033[0m\n"
+    printf " ╚═ $(ccode "th a dev s b")         : Opens the AWS console for the above account & role.\n"
+}
+
+print_kube_help() {
+    print "\033c"
+    create_header "th kube | k"
+    printf "\033[1mLogin to our Kubernetes clusters.\033[0m\n\n"
+    printf "Usage: \033[1mth kube [options] | k\033[0m\n"
+    printf " ╚═ \033[1mth k\033[0m                   : Open interactive login.\n"
+    printf " ╚═ \033[1mth k <cluster>\033[0m         : Quick kube log-in, Where:\n"
+    printf "    ╚═ \033[1m<cluster>\033[0m is an abbreviated cluster name e.g. dev, cpg etc..\n\n"
+    printf "Examples:\n"
+    printf " ╚═ $(ccode "th k dev")             : logs you into \033[0;32maslive-dev-eks-blue.\033[0m\n"
+}
 # ========================================================================================================================
 #                                                            Extras
 # ========================================================================================================================
--- th/th.sh
+++ th/th.sh
@@ -26,15 +26,7 @@ th(){
   case "$1" in
     kube|k)
       if [[ "$2" == "-h" ]]; then
-        print "\033c"
-        create_header "th kube | k"
-        printf "Login to our Kubernetes clusters.\n\n"
-        printf "Usage: \033[1mth kube [options] | k\033[0m\n"
-        printf " ╚═ \033[1mth k\033[0m                   : Open interactive login.\n"
-        printf " ╚═ \033[1mth k <cluster>\033[0m         : Quick kube log-in, Where:\n"
-        printf "    ╚═ \033[1m<cluster>\033[0m is an abbreviated cluster name e.g. dev, cpg etc..\n\n"
-        printf "Examples:\n"
-        printf " ╚═ $(ccode "th k dev")             : logs you into \033[0;32maslive-dev-eks-blue.\033[0m\n"
+        print_kube_help
       else
         shift
         kube_login "$@"
@@ -60,19 +52,7 @@ th(){
       ;;
     aws|a)
       if [[ "$2" == "-h" ]]; then
-        print "\033c"
-        create_header "th aws | a"
-        printf "Login to our AWS accounts.\n\n"
-        printf "Usage: \033[1mth aws [options] | a\033[0m\n"
-        printf " ╚═ \033[1mth a\033[0m                   : Open interactive login.\n"
-        printf " ╚═ \033[1mth a <account> <s> <b>\033[0m : Quick aws log-in, Where:\n"
-        printf "    ╚═ \033[1m<account>\033[0m is an abbreviated account name e.g. dev, cpg etc...\n"
-        printf "    ╚═ \033[1m<s>\033[0m is an optional arg which logs you in with the account's sudo role\n"
-        printf "    ╚═ \033[1m<b>\033[0m is another optional arg which opens the aws console.\n\n"
-        printf "Examples:\n"
-        printf " ╚═ $(ccode "th a dev")             : logs you into \033[0;32myl-development\033[0m as \033[1;4;32mdev\033[0m\n"
-        printf " ╚═ $(ccode "th a dev s")           : logs you into \033[0;32myl-development\033[0m as \033[1;4;32msudo_dev\033[0m\n"
-        printf " ╚═ $(ccode "th a dev s b")         : Opens the AWS console for the above account & role.\n"
+        print_aws_help
       else
         shift
         aws_login "$@"
@@ -84,19 +64,7 @@ th(){
       ;;
     database|d)
       if [[ "$2" == "-h" ]]; then
-        print "\033c"
-        create_header "th database | d"
-        printf "Connect to our databases (RDS and MongoDB).\n\n"
-        printf "Usage: \033[1mth database [options] | d\033[0m\n"
-        printf " ╚═ \033[1mth d\033[0m                   : Open interactive database selection.\n"
-        printf " ╚═ \033[1mth d <db-env> <port>\033[0m   : Quick database connect, Where:\n"
-        printf "    ╚═ \033[1m<db-env>\033[0m is an abbreviation for an RDS or Mongo database, using the format:\n"
-        printf "                <dbtype-env>. e.g. \033[1mr-dev\033[0m would connect to the \033[1mdev RDS cluster\033[0m\n"
-        printf "    ╚═ \033[1m<port>\033[0m   is An optional arg that allows you to specify a\n"
-        printf "                custom port for connection reuse in GUIs\n\n"
-        printf "Examples:\n"
-        printf " ╚═ $(ccode "th d r-dev")           : connects to the \033[0;32mdb-dev-aurora-postgres-1\033[0m.\n"
-        printf " ╚═ $(ccode "th d m-prod")          : connects to \033[0;32mmongodb-YLProd-Cluster-1\033[0m.\n"
+        print_db_help
       else
         shift
         db_login "$@"
